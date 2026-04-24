5bbc4973514a92644c35f67bfdc96a8aa2113edd
Adriel Cassimiro do Nascimento <acnascimento-sys@proton.me>
2026-04-23 21:08:28 -0300

    .
    .

---
+++ python/Aprendendo Python/salva1doc.py
@@ -0,0 +1,21 @@
+#salva 1doc.py
+
+from pyautogui import *
+import pyautogui
+
+#abrir o processo
+pyautogui.click (182,390)
+
+#opção de imprimir
+pyautogui.click (1169,181)
+#opção arvore de processo
+pyautogui.click (1141,252)
+#botão baixar
+pyautogui.click (353,387)
+#icone salvar navegador
+pyautogui.click (1235,97)
+
+
+
+
+
